81d7ae2292cb3be80d76ec295efd71ab450e5652
SPLAT Admin <Admin@usesplat.com>
2025-07-31 12:22:58 -0600

    Fix lib/index.ts explicit exports for staging build

---
+++ lib/index.ts
@@ -0,0 +1,5 @@
+export { verifyCaptcha } from './verifyCaptcha';
+export { sendEmail } from './sendEmail';
+export { validateForm } from './validateForm';
+export { sendSuccess, sendError } from './apiResponse';
+export { splatApiHandler } from './splatApiHandler';
