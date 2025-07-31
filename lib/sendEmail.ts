41c17cc7d244f78216d61fb2783f7a64e77f50ff
SPLAT Admin <Admin@usesplat.com>
2025-07-31 13:53:12 -0600

    🐾 Force named export + correct import for sendEmail (fix callable issue)

--- lib/sendEmail.ts
+++ lib/sendEmail.ts
@@ -9,6 +9,8 @@ export interface EmailParams {
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail({ to, subject, html, from }: EmailParams): Promise<{ success: boolean }> {
-  await resend.emails.send({ from: from || 'default@usesplat.com', to, subject, html });
+  await resend.emails.send({ from: from || "default@usesplat.com", to, subject, html });
+  return { success: true };
+}  await resend.emails.send({ from: from || 'default@usesplat.com', to, subject, html });
   return { success: true };
 }
