de1ce34d900175a223a1a5aecec170a30241835b
SPLAT Admin <Admin@usesplat.com>
2025-07-31 15:16:40 -0600

    🐾 Add proper exports for AmbassadorForm & ContactForm in formTypes.ts

---
+++ types/formTypes.ts
@@ -0,0 +1,20 @@
+export type AmbassadorForm = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  dob: string;
+  city: string;
+  state: string;
+  social_media_handles: string;
+  number_of_followers: number;
+  qualifications_why: string;
+  referral?: string;
+  captchaToken: string;
+};
+
+export type ContactForm = {
+  name: string;
+  email: string;
+  message: string;
+  captchaToken: string;
+};
