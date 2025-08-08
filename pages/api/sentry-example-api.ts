40e5745a9ef282c01a764d34b45ee9bce21cf210
SPLAT Admin <Admin@usesplat.com>
2025-08-08 12:12:13 -0600

    fix(ts): type Next.js API route params

--- pages/api/sentry-example-api.ts
+++ pages/api/sentry-example-api.ts
@@ -1,12 +1,10 @@
-// Custom error class for Sentry testing
-class SentryExampleAPIError extends Error {
-  constructor(message: string | undefined) {
-    super(message);
-    this.name = "SentryExampleAPIError";
-  }
-}
+import type { NextApiRequest, NextApiResponse } from "next";
+
 // A faulty API route to test Sentry's error monitoring
-export default function handler(_req, res) {
-throw new SentryExampleAPIError("This error is raised on the backend called by the example page.");
-res.status(200).json({ name: "John Doe" });
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+  throw new SentryExampleAPIError(
+    "This error is raised on the backend called by the example page."
+  );
+  // Unreachable, but types are happy either way:
+  // res.status(200).json({ name: "John Doe" });
 }
